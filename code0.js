44bfe08f0989d86b5d6d5a51faa0beeecccb8674
Carlos López Barrera <52845915+binarsdev@users.noreply.github.com>
2021-11-20 23:42:29 -0600

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,20 @@
+gdjs.indexCode = {};
+
+gdjs.indexCode.conditionTrue_0 = {val:false};
+gdjs.indexCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.indexCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.indexCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.indexCode.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['indexCode'] = gdjs.indexCode;
